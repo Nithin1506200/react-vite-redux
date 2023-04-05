44a4196e56e99df2df703cccab30844e94888b3d
Nitin N <nitin.n@saarthi.ai>
2023-04-05 11:13:48 +0530

    update storybooks

--- src/components/generic/dummy/DummyComponent.tsx
+++ src/components/generic/dummy/DummyComponent.tsx
@@ -7,12 +7,14 @@ import { useAppSelector } from '@/hooks/store/useAppSelector';
 export default function Dummy() {
   // console.log(import.meta.env.VITE_ENABLE_DEV_TOOLS);
   const x = useAppSelector((state) => state.dummyReducer.dummy1);
+  const dummy2 = useAppSelector((state) => state.dummyReducer.dummy2);
   const dispatch = useAppDispatch();
 
   return (
     <>
       <h1>{import.meta.env.VITE_ENABLE_DEV_TOOLS}</h1>
       <h1>{x.value}</h1>
+      <h1>{dummy2.value}</h1>
       <button
         onClick={() => {
           dispatch(changeName('name changed'));
