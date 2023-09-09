401f563f4af013de884e86c9b18290202063ff3f
nithin <nithingowdan77@gmail.com>
2023-09-09 11:24:09 +0530

    changes in storybooks

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,6 @@
 import type { Preview } from '@storybook/react';
-// import '../src/css/*';
+import '../src/css/colors.scss';
+import '../src/index.css';
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
